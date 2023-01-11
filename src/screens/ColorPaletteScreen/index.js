98eea795dabefb040ce574b67f1d098841d09a00
Drew André <drewjamesandre@gmail.com>
2023-01-10 18:54:44 -0800

    update color gradient

--- src/screens/ColorPaletteScreen/index.js
+++ src/screens/ColorPaletteScreen/index.js
@@ -1,10 +1,34 @@
 import React from "react";
-import { View, TouchableOpacity, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button, Image } from "react-native";
 import { connect } from "react-redux";
-import ColorPicker from "react-native-wheel-color-picker";
+import ColorPicker from "../../vendor/ColorPicker";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { TapGestureHandler } from "react-native-gesture-handler";
+import * as Haptics from "expo-haptics";
+
+const hex2Rgb = (hex) => {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+};
 
 function ColorPaletteScreen({ navigation, route }) {
   const pickerRef = React.useRef(null);
+  const [frontColorSelectedState, setFrontColorSelectedState] =
+    React.useState(true);
+  const frontColorSelected = useSharedValue(1);
+  const frontColor = useSharedValue("#fff");
+  const backColor = useSharedValue("#f00");
+
   React.useEffect(() => {
     navigation.setOptions({
       headerRight: () => {
@@ -27,24 +51,112 @@ function ColorPaletteScreen({ navigation, route }) {
     pickerRef.current = ref;
   }
 
+  function handleFrontColorPress() {
+    frontColorSelected.value = 1;
+    setFrontColorSelectedState(true);
+  }
+
+  function handleBackColorPress() {
+    frontColorSelected.value = 0;
+    setFrontColorSelectedState(false);
+  }
+
+  function onFrontColorChange(hex, a) {
+    const { r, g, b } = hex2Rgb(hex);
+    frontColor.value = `rgba(${r}, ${g}, ${b}, ${a})`;
+  }
+
+  function onBackColorChange(hex, a) {
+    const { r, g, b } = hex2Rgb(hex);
+    backColor.value = `rgba(${r}, ${g}, ${b}, ${a})`;
+  }
+
+  function onInteractionStart() {
+    Haptics.selectionAsync();
+  }
+
+  const animatedFrontColorStyles = useAnimatedStyle(() => {
+    return {
+      flex: 1,
+      overflow: "hidden",
+      zIndex: frontColorSelected.value ? 5 : 0,
+      transform: [{ scale: withSpring(frontColorSelected.value ? 1.2 : 1) }],
+      borderRadius: 15,
+    };
+  });
+
+  const animatedBackColorStyles = useAnimatedStyle(() => {
+    return {
+      flex: 1,
+      overflow: "hidden",
+      zIndex: frontColorSelected.value ? 0 : 5,
+      transform: [{ scale: withSpring(frontColorSelected.value ? 1 : 1.2) }],
+      borderRadius: 15,
+    };
+  });
+
+  const animatedBackBackgroundStyles = useAnimatedStyle(() => {
+    return {
+      flex: 1,
+      backgroundColor: backColor.value,
+    };
+  });
+
+  const animatedFrontBackgroundStyles = useAnimatedStyle(() => {
+    return {
+      flex: 1,
+      backgroundColor: frontColor.value,
+    };
+  });
+
   return (
     <View style={styles.wrapper}>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.frontColor} />
-        <TouchableOpacity style={styles.backColor} />
+        <TapGestureHandler onActivated={handleFrontColorPress}>
+          <Animated.View style={animatedFrontColorStyles}>
+            <Image
+              source={require("../../assets/alpha.jpg")}
+              style={styles.alphaBackground}
+            />
+            <Animated.View style={animatedFrontBackgroundStyles} />
+          </Animated.View>
+        </TapGestureHandler>
+        <TapGestureHandler onActivated={handleBackColorPress}>
+          <Animated.View style={animatedBackColorStyles}>
+            <Image
+              source={require("../../assets/alpha.jpg")}
+              style={styles.alphaBackground}
+            />
+            <Animated.View style={animatedBackBackgroundStyles} />
+          </Animated.View>
+        </TapGestureHandler>
       </View>
-      <ColorPicker
-        ref={setPickerRef}
-        // color={this.state.currentColor}
-        // onColorChange={this.onColorChange}
-        // onColorChangeComplete={this.onColorChangeComplete}
-        thumbSize={40}
-        sliderSize={30}
-        row={false}
-        swatches={false}
-        // swatchesLast={this.state.swatchesLast}
-        // swatches={this.state.swatchesEnabled}
-      />
+      <View style={styles.colorPickerContainer}>
+        <ColorPicker
+          ref={setPickerRef}
+          color={frontColorSelectedState ? frontColor.value : backColor.value}
+          onColorChange={
+            frontColorSelectedState ? onFrontColorChange : onBackColorChange
+          }
+          onInteractionStart={onInteractionStart}
+          // onColorChangeComplete={this.onColorChangeComplete}
+          thumbSize={40}
+          sliderSize={30}
+          row={false}
+          shadeSliderThumb
+          palette={[
+            "#ffffff",
+            "#FFF6ED",
+            "#ff0000",
+            "#ffa500",
+            "#ffff00",
+            "#008000",
+            "#0000ff",
+            "#4b0082",
+            "#ee82ee",
+          ]}
+        />
+      </View>
     </View>
   );
 }
@@ -56,25 +168,22 @@ const styles = StyleSheet.create({
     margin: 15,
     padding: 15,
     backgroundColor: "#121212",
+    justifyContent: "space-around",
   },
-  frontColor: {
-    flex: 1,
-    backgroundColor: "red",
-    height: 80,
-    borderTopLeftRadius: 30,
-    borderBottomLeftRadius: 30,
+  alphaBackground: {
+    resizeMode: "cover",
+    ...StyleSheet.absoluteFill,
   },
-  backColor: {
-    flex: 1,
-    backgroundColor: "red",
-    height: 80,
-    borderTopRightRadius: 30,
-    borderBottomRightRadius: 30,
+  colorPickerContainer: {
+    alignSelf: "center",
+    width: "50%",
+    height: "70%",
   },
   buttonContainer: {
     alignSelf: "center",
     flexDirection: "row",
-    width: "75%",
+    height: 80,
+    width: "80%",
   },
   warningLabel: {
     color: "#ff6700",
